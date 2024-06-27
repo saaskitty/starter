2e4fda4171b8ba4806994d90c93912de3d3cbc99
saaskitten <founder@saaskitty.com>
2024-06-27 11:35:55 +0800

    move `configSchema` to `config.ts`

---
+++ app/.server/config.ts
@@ -0,0 +1,119 @@
+import type { AppConfig } from "saaskitty/server";
+import { z } from "saaskitty/validator";
+
+/**
+ * The application config.
+ */
+export type Config = AppConfig<typeof configSchema>;
+
+/**
+ * The application config schema.
+ */
+export const configSchema = {
+	/**
+	 * The application description.
+	 *
+	 * @default "The starter template for quickly launching your full-stack web application."
+	 */
+	APP_DESCRIPTION: z
+		.string()
+		.default(
+			"The starter template for quickly launching your full-stack web application.",
+		),
+
+	/**
+	 * The application environment.
+	 *
+	 * @default "development"
+	 */
+	APP_ENV: z.string().default("development"),
+
+	/**
+	 * The application name.
+	 *
+	 * @default "starter"
+	 */
+	APP_NAME: z.string().default("starter"),
+
+	/**
+	 * The application version.
+	 *
+	 * @default "0.1.0"
+	 */
+	APP_VERSION: z.string().default("0.1.0"),
+
+	/**
+	 * The secrets used to sign the cookies.
+	 */
+	COOKIE_SECRETS: z
+		.string()
+		.transform((val) => val.split(",").map((secret) => secret.trim()))
+		.refine(
+			(arr) => arr.length > 0 && arr.every((secret) => secret.length > 0),
+			{
+				message: "must be a comma-separated list of non-empty strings",
+			},
+		)
+		.transform((arr) => arr as string[]),
+
+	/**
+	 * The secret used to sign the CSRF token.
+	 */
+	CSRF_SECRET: z.string(),
+
+	/**
+	 * The NodeJS environment. Setting the environment to "production" generally ensures that:
+	 *
+	 * - logging is kept to a minimum, essential level
+	 * - more caching levels take place to optimize performance
+	 *
+	 * @default "development"
+	 */
+	NODE_ENV: z
+		.enum(["development", "production", "test"])
+		.default("development"),
+
+	/**
+	 * The server hostname.
+	 *
+	 * @default "0.0.0.0"
+	 */
+	HOST: z.string().default("0.0.0.0"),
+
+	/**
+	 * The server port number.
+	 *
+	 * @default "8000"
+	 */
+	PORT: z.string().default("8000"),
+
+	/**
+	 * The primary cache connection URL.
+	 */
+	PRIMARY_CACHE_URL: z.string(),
+
+	/**
+	 * The primary database connection URL.
+	 */
+	PRIMARY_DB_URL: z.string(),
+
+	/**
+	 * The primary SMTP connection URL.
+	 */
+	PRIMARY_SMTP_URL: z.string(),
+
+	/**
+	 * The primary object storage bucket.
+	 */
+	PRIMARY_BUCKET: z.string(),
+
+	/**
+	 * The Redis pubsub connection URL.
+	 */
+	PUBSUB_URL: z.string(),
+
+	/**
+	 * The Redis queue connection URL.
+	 */
+	QUEUE_URL: z.string(),
+};
--- app/.server/main.ts
+++ app/.server/main.ts
@@ -1,6 +1,5 @@
 import { getI18nInitOptions } from "saaskitty/i18n";
 import {
-	type AppConfig,
 	type AppInstance,
 	AppStorage,
 	type BootstrapOpts,
@@ -9,9 +8,9 @@ import {
 	getAppCache,
 	getAppMailer,
 } from "saaskitty/server";
-import { z } from "saaskitty/validator";
 import { getChannels } from "#app/.server/channels/index.js";
 import { getCommands } from "#app/.server/commands/index.js";
+import { configSchema } from "#app/.server/config.js";
 import { getCrons } from "#app/.server/crons/index.js";
 import { getDatabases } from "#app/.server/databases/index.js";
 import { getEvents } from "#app/.server/events/index.js";
@@ -32,124 +31,6 @@ export interface App
 		typeof getMailers,
 		typeof getStorages
 	> {}
-
-/**
- * The application config.
- */
-export type Config = AppConfig<typeof configSchema>;
-
-/**
- * The application config schema.
- */
-const configSchema = {
-	/**
-	 * The application description.
-	 *
-	 * @default "The starter template for quickly launching your full-stack web application."
-	 */
-	APP_DESCRIPTION: z
-		.string()
-		.default(
-			"The starter template for quickly launching your full-stack web application.",
-		),
-
-	/**
-	 * The application environment.
-	 *
-	 * @default "development"
-	 */
-	APP_ENV: z.string().default("development"),
-
-	/**
-	 * The application name.
-	 *
-	 * @default "starter"
-	 */
-	APP_NAME: z.string().default("starter"),
-
-	/**
-	 * The application version.
-	 *
-	 * @default "0.1.0"
-	 */
-	APP_VERSION: z.string().default("0.1.0"),
-
-	/**
-	 * The secrets used to sign the cookies.
-	 */
-	COOKIE_SECRETS: z
-		.string()
-		.transform((val) => val.split(",").map((secret) => secret.trim()))
-		.refine(
-			(arr) => arr.length > 0 && arr.every((secret) => secret.length > 0),
-			{
-				message:
-					"COOKIE_SECRETS must be a comma-separated list of non-empty strings",
-			},
-		)
-		.transform((arr) => arr as string[]),
-
-	/**
-	 * The secret used to sign the CSRF token.
-	 */
-	CSRF_SECRET: z.string(),
-
-	/**
-	 * The NodeJS environment. Setting the environment to "production" generally ensures that:
-	 *
-	 * - logging is kept to a minimum, essential level
-	 * - more caching levels take place to optimize performance
-	 *
-	 * @default "development"
-	 */
-	NODE_ENV: z
-		.enum(["development", "production", "test"])
-		.default("development"),
-
-	/**
-	 * The server hostname.
-	 *
-	 * @default "0.0.0.0"
-	 */
-	HOST: z.string().default("0.0.0.0"),
-
-	/**
-	 * The server port number.
-	 *
-	 * @default "8000"
-	 */
-	PORT: z.string().default("8000"),
-
-	/**
-	 * The primary cache connection URL.
-	 */
-	PRIMARY_CACHE_URL: z.string(),
-
-	/**
-	 * The primary database connection URL.
-	 */
-	PRIMARY_DB_URL: z.string(),
-
-	/**
-	 * The primary SMTP connection URL.
-	 */
-	PRIMARY_SMTP_URL: z.string(),
-
-	/**
-	 * The primary object storage bucket.
-	 */
-	PRIMARY_BUCKET: z.string(),
-
-	/**
-	 * The Redis pubsub connection URL.
-	 */
-	PUBSUB_URL: z.string(),
-
-	/**
-	 * The Redis queue connection URL.
-	 */
-	QUEUE_URL: z.string(),
-};
 
 /**
  * Get the application caches.
